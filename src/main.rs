8b3da788cebae9d1a3a621411a23980e72a9fa1a
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-08-04 15:18:14 -0700

    remove yml file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use anyhow::{bail, Result};
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::Cursor;
+use std::num;
 
 fn main() -> Result<()> {
     // Parse arguments
@@ -23,14 +25,34 @@ fn main() -> Result<()> {
             #[allow(unused_variables)]
             let page_size = u16::from_be_bytes([header[16], header[17]]);
 
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            eprintln!("Logs from your program will appear here!");
+            let mut page_type = [0u8; 3];
+            let _ = file.read_exact(&mut page_type);
+            // println!("page type bytes {page_type:?}");
 
-            // Uncomment this block to pass the first stage
+            let mut num_of_cells = [0u8; 2];
+            let _ = file.read_exact(&mut num_of_cells);
+            let num_of_cells = u16::from_be_bytes(num_of_cells);
+
+            print!("number of tables: {num_of_cells}");
             println!("database page size: {page_size}");
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
 
     Ok(())
+}
+
+
+#[derive(Debug)]
+enum PageType {
+    TableLeaf,
+}
+
+impl PageType {
+    fn from_byte(byte: &[u8]) -> PageType {
+        match byte[0] {
+            13 => PageType::TableLeaf,
+            _ => todo!()
+        }
+    }
 }
