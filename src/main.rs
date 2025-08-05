71fadfaec907940726f0ea32da05d6eadd2c77a5
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2025-08-05 15:02:35 -0700

    Add page header struct and parse page header

--- src/main.rs
+++ src/main.rs
@@ -44,23 +44,29 @@ fn main() -> Result<()> {
             let mut leaf_header = [0; 8];
             let mut interior_header = [0; 12];
 
-            let mut page_type_buf = [0u8; 3];
+            let mut page_type_buf = [0u8; 1];
             let _ = file.read_exact(&mut page_type_buf);
 
             let page_type = PageType::from_bytes(&page_type_buf);
 
-            match page_type {
+            let page_header = match page_type {
                 PageType::TableLeaf | PageType::IndexLeaf => {
-                    leaf_header[0..3].copy_from_slice(&page_type_buf);
-                    file.read_exact(&mut leaf_header[3..])?
+                    leaf_header[0..1].copy_from_slice(&page_type_buf);
+                    file.read_exact(&mut leaf_header[1..])?;
+                    PageHeader::try_from(&leaf_header[..])
                 }
                 PageType::TableInterior | PageType::IndexInterior => {
-                    interior_header[0..3].copy_from_slice(&page_type_buf);
-                    file.read_exact(&mut interior_header[3..])?
+                    interior_header[0..1].copy_from_slice(&page_type_buf);
+                    file.read_exact(&mut interior_header[1..])?;
+                        todo!()
                 }
-            }
+            };
 
-            println!("next {leaf_header:?}");
+            // let mut next = [0; 40];
+            //
+            // file.read_exact(&mut next);
+
+            println!("next {page_header:?}");
         }
         _ => bail!("Missing or invalid command passed: {}", command),
     }
@@ -194,5 +200,53 @@ impl TryFrom<&[u8]> for DatabaseHeader {
             version_valid_for_number,
             sqlite_version,
         })
+    }
+}
+
+#[derive(Debug)]
+struct PageHeader {
+    page_type: PageType,
+    first_free_block: u16,
+    cell_count: u16,
+    cell_content_area: u16,
+    fragment_free_bytes: u8,
+}
+
+impl TryFrom<&[u8]> for PageHeader {
+    type Error = anyhow::Error;
+
+    fn try_from(mut bytes: &[u8]) -> Result<Self> {
+        println!("bytes ;{bytes:?}");
+        if bytes.len() < 8 {
+            return Err(anyhow!("Header is too short"));
+        }
+
+        let mut page_type = [0; 1];
+        let mut first_free_block = [0; 2];
+        let mut cell_count = [0; 2];
+        let mut cell_content_area = [0; 2];
+        let mut fragment_free_bytes = [0; 1];
+
+        let _ = bytes.read_exact(&mut page_type);
+        let _ = bytes.read_exact(&mut first_free_block);
+        let _ = bytes.read_exact(&mut cell_count);
+        let _ = bytes.read_exact(&mut cell_content_area);
+        let _ = bytes.read_exact(&mut fragment_free_bytes);
+
+        let page_type = PageType::from_bytes(&page_type);
+        let first_free_block = u16::from_be_bytes(first_free_block);
+        let cell_count = u16::from_be_bytes(cell_count);
+        let cell_content_area = u16::from_be_bytes(cell_content_area);
+        let fragment_free_bytes = u8::from_be_bytes(fragment_free_bytes);
+
+        let page_header = PageHeader {
+            page_type,
+            first_free_block,
+            cell_count,
+            cell_content_area,
+            fragment_free_bytes
+        };
+
+        Ok(page_header)
     }
 }
